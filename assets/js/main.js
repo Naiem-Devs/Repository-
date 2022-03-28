a28217ed27e463396a91836db037aff6fe7b446e
Developer-Nasim <ajnasim72@gmail.coom>
2022-03-28 23:40:36 +0600

    added other screens and menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,27 +8,15 @@
 
 
   // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
+  $(".detailses_items_carousel").owlCarousel({ 
+    items: 1,
+    // navText: [
+    //   '<i class="fa fa-angle-left"></i>',
+    //   '<i class="fa fa-angle-right"></i>'
+    // ],
     nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
+    dots: true, 
+    autoHeight:true
   });
 
   /* magnificPopup img view */
